cfa5a1f961847b2cc3e719a209c04b90494321a1
daykalif <1038200595@qq.com>
2023-02-19 14:54:08 +0800

    feat:编写登陆表单

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Button, Form, Input } from 'antd';
+import './login.css';
 
-const App: React.FC = () => {
-  return (<div> hello world </div>);
+interface Props {
+
 }
+
+const onFinish = (values: any) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log('Failed:', errorInfo);
+};
+
+const App: React.FC = (props: Props) => (
+  <div className='login-page'>
+    <Form
+      name="basic"
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 18 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="密码"
+        name="password"
+        rules={[{ required: true, message: '请输入登陆密码！' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          登陆
+        </Button>
+      </Form.Item>
+    </Form>
+  </div>
+);
 
 export default App;
